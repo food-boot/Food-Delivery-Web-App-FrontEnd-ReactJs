63be1227ed9cc04a5f24068970f26b3c945b3c87
wathsara daluwatta <wvd.51461@gmail.com>
2019-06-05 16:16:56 +0530

    Added some Components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 import {BrowserRouter as Router , Switch , Route} from "react-router-dom"
 import SignIn from "./Cilent/Authentication/SignIn/SignIn";
 import SignUp from "./Cilent/Authentication/SignUp/SignUp";
+import Dashbord from "./Cilent/Dashboard/Default"
+import Forms from "./Cilent/Forms/FormsElements"
+import Nav from "./Cilent/layout/AdminLayout/Breadcrumb"
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">        
-        <Route path="/signIn" component={SignIn}/>
+      <div className="App">  
+        <Nav/>      
+        <Route path="/signIn" component={SignIn}/>        
         <Route path="/signUp" component={SignUp}/>
+        <Route path="/dashboard" component={Dashbord}/>
+        <Route path="/form" component={Forms}/>
       </div>
       </Router>
     );
