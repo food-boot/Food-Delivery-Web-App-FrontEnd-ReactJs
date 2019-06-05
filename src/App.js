4b6b1043ab498b9a74a25dbe82d49fcc1ca4abd4
wathsara daluwatta <wvd.51461@gmail.com>
2019-06-05 19:41:36 +0530

    Creted Charts Tab and Fixed Isuues

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import SignIn from "./Cilent/Authentication/SignIn/SignIn";
 import SignUp from "./Cilent/Authentication/SignUp/SignUp";
 import Dashbord from "./Cilent/Dashboard/Default"
 import Forms from "./Cilent/Forms/FormsElements"
+import Charts from "./Cilent/Charts/Nvd3Chart/index"
 import Nav from "./Cilent/layout/AdminLayout/Breadcrumb"
 class App extends Component {
   render() {
     return (
       <Router>
       <div className="App">  
-        <Nav/>      
+        <Nav/>   
+        <br/><br/>   
         <Route path="/signIn" component={SignIn}/>        
         <Route path="/signUp" component={SignUp}/>
         <Route path="/dashboard" component={Dashbord}/>
         <Route path="/form" component={Forms}/>
+        <Route path="/chart" component={Charts}/>
       </div>
       </Router>
     );
